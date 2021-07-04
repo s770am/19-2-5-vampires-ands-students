0b36371878b8e79dfb20eb9df5773d0aaa4cdacb
s770am <s770am@gmail.com>
2021-07-04 09:12:44 -0400

    set up people class

--- people.rb
+++ people.rb
@@ -1,11 +1,23 @@
-class Student
+class People
+
+    def initialize(name)
+    @name = name
+    end
+    def greeting
+    puts "hi my name is #{@name}"
+    end
+    
+    end
+
+class Student < People
 def learn 
 puts "i get it"
 end
 end
 
-class Instructor
+class Instructor < People
 def teach
     puts "Everything in Ruby is an Object"
 end
 end
+
